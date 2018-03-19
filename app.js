711057c34eb1b06792a3d20328471c1ab20a6a93
nafhta <nafhta@gmail.com>
2018-03-19 18:28:29 -0500

    fix

--- app.js
+++ app.js
@@ -137,7 +137,8 @@ function GetTextMessage(text_response)
 function GetHotelMessage()
 {
     var hotel_message =
-        "message":{
+        {
+            "message":{
             "attachment":{
               "type":"template",
               "payload":{
@@ -169,7 +170,8 @@ function GetHotelMessage()
                 ]
               }
             }
-          };
+          }
+      };
 
     return hotel_message;
 }
@@ -177,72 +179,73 @@ function GetHotelMessage()
 function GetAirMessage()
 {
     var flight_message = 
-    "message": {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "airline_boardingpass",
-        "intro_message": "You are checked in.",
-        "locale": "en_US",
-        "boarding_pass": [
-          {
-            "passenger_name": "SMITH\/NICOLAS",
-            "pnr_number": "CG4X7U",
-            "seat": "74J",            
-            "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
-            "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
-            "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
-            "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
-            "auxiliary_fields": [
+    {"message": {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "airline_boardingpass",
+            "intro_message": "You are checked in.",
+            "locale": "en_US",
+            "boarding_pass": [
               {
-                "label": "Terminal",
-                "value": "T1"
-              },
-              {
-                "label": "Departure",
-                "value": "30OCT 19:05"
+                "passenger_name": "SMITH\/NICOLAS",
+                "pnr_number": "CG4X7U",
+                "seat": "74J",            
+                "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
+                "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
+                "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
+                "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
+                "auxiliary_fields": [
+                  {
+                    "label": "Terminal",
+                    "value": "T1"
+                  },
+                  {
+                    "label": "Departure",
+                    "value": "30OCT 19:05"
+                  }
+                ],
+                "secondary_fields": [
+                  {
+                    "label": "Boarding",
+                    "value": "18:30"
+                  },
+                  {
+                    "label": "Gate",
+                    "value": "D57"
+                  },
+                  {
+                    "label": "Seat",
+                    "value": "74J"
+                  },
+                  {
+                    "label": "Sec.Nr.",
+                    "value": "003"
+                  }
+                ],
+                "flight_info": {
+                  "flight_number": "KL0642",
+                  "departure_airport": {
+                    "airport_code": "JFK",
+                    "city": "New York",
+                    "terminal": "T1",
+                    "gate": "D57"
+                  },
+                  "arrival_airport": {
+                    "airport_code": "AMS",
+                    "city": "Amsterdam"
+                  },
+                  "flight_schedule": {
+                    "departure_time": "2016-01-02T19:05",
+                    "arrival_time": "2016-01-05T17:30"
+                  }
+                }
               }
-            ],
-            "secondary_fields": [
-              {
-                "label": "Boarding",
-                "value": "18:30"
-              },
-              {
-                "label": "Gate",
-                "value": "D57"
-              },
-              {
-                "label": "Seat",
-                "value": "74J"
-              },
-              {
-                "label": "Sec.Nr.",
-                "value": "003"
-              }
-            ],
-            "flight_info": {
-              "flight_number": "KL0642",
-              "departure_airport": {
-                "airport_code": "JFK",
-                "city": "New York",
-                "terminal": "T1",
-                "gate": "D57"
-              },
-              "arrival_airport": {
-                "airport_code": "AMS",
-                "city": "Amsterdam"
-              },
-              "flight_schedule": {
-                "departure_time": "2016-01-02T19:05",
-                "arrival_time": "2016-01-05T17:30"
-              }
-            }
+            ]
           }
-        ]
+        }
       }
-    }
-  };
+    };
 
   return flight_message;
 
