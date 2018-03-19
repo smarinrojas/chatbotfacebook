0428db266b93070fcb949ba4d1154a51d897aa10
nafhta <nafhta@gmail.com>
2018-03-19 18:42:54 -0500

    removing urls

--- app.js
+++ app.js
@@ -150,15 +150,15 @@ function GetHotelMessage()
                     "subtitle":"We've got the right hat for everyone.",
                     "default_action": {
                       "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                      "url": "",
                       "messenger_extensions": true,
                       "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                      "fallback_url": ""
                     },
                     "buttons":[
                       {
                         "type":"web_url",
-                        "url":"https://petersfancybrownhats.com",
+                        "url":"",
                         "title":"View Website"
                       },{
                         "type":"postback",
